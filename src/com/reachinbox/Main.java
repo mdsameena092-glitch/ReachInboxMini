b977d91b4ae0a0ba63dbd23c891417592849574c
mdsameena092-glitch <mdsameena092@gmail.com>
2026-01-17 20:34:47 +0530

    Add files via upload

---
+++ src/com/reachinbox/Main.java
@@ -0,0 +1,20 @@
+package com.reachinbox;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Sample User";
+		String email = "sample@email.com";
+		String message = "Hello from ReachInox";
+		
+		System.out.println("Name: " +name);
+		System.out.println("Email:" +email);
+		System.out.println("Message:"+message);
+		
+		System.out.println("Email sent successfully (simulation)");
+
+	}
+
+}
